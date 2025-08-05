c33b54cd62e1b77b19aaf754a0cefbb342bf08d9
ㄷㅌ <62054524+exte0427@users.noreply.github.com>
2025-08-05 10:06:45 +0900

    TEST TEST

--- lib/main.dart
+++ lib/main.dart
@@ -10,53 +10,10 @@ void main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  //runApp(const ProviderScope(child: BloomApp()));
-  runApp(
-    const MaterialApp(
-      home: UnityDemoScreen(),
-    ),
-  );
+  runApp(const ProviderScope(child: BloomApp()));
+
 }
 
-class UnityDemoScreen extends StatefulWidget {
-  const UnityDemoScreen({Key? key}) : super(key: key);
-
-  @override
-  State<UnityDemoScreen> createState() => _UnityDemoScreenState();
-}
-
-class _UnityDemoScreenState extends State<UnityDemoScreen> {
-  static final GlobalKey<ScaffoldState> _scaffoldKey =
-  GlobalKey<ScaffoldState>();
-  UnityWidgetController? _unityWidgetController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      body: SafeArea(
-        bottom: false,
-        child: WillPopScope(
-          onWillPop: () async {
-            // Pop the category page if Android back button is pressed.
-            return true;
-          },
-          child: Container(
-            color: Colors.yellow,
-            child: UnityWidget(
-              onUnityCreated: onUnityCreated,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  // Callback that connects the created controller to the unity controller
-  void onUnityCreated(controller) {
-    _unityWidgetController = controller;
-  }
-}
 
 class BloomApp extends StatelessWidget {
   const BloomApp({super.key});
